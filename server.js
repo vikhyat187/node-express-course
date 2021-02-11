c150c974e4946ee53d9cd042b0d087cd0135857e
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2021-02-11 20:46:49 +0530

    set up express server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
 
-app.listen(4000,()=>{
-    console.log('our app is running ');
+app.listen(8000,()=>{
+    console.log("server is running")
 })
