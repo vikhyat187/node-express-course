36a97211987cf89006fe59bb1403db3200a0d016
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2021-02-12 12:55:28 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express=require('express');
 const app=express();
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
 ]
+app.get('/users',function(req,res){
+    res.json({
+        success:true,
+        message:'successfully got users. Nice!',
+        users:mockUserData
+    })
+})
+app.get('/users/:id',function(req,res){
+    console.log(req.params.id)
+    res.json({
+        success:true,
+        message:'got one user',
+        user:req.params.id
+    })
+})
 
 app.post('/login',function(req,res){
     const username= req.body.username;
@@ -28,21 +44,7 @@ app.post('/login',function(req,res){
         })
     }
 })
-app.get('/users',function(req,res){
-    res.json({
-        success:true,
-        message:'successfully got users. Nice!',
-        users:mockUserData
-    })
-})
-app.get('/users/:id',function(req,res){
-    console.log(req.params.id)
-    res.json({
-        success:true,
-        message:'got one user',
-        user:req.params.id
-    })
-})
+
 app.listen(8000,()=>{
     console.log("server is running")
 })
